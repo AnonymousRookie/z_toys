afbf31b5d19f0b00ec12c59e75311918c62e5a22
zhangjie <357688981@qq.com>
2021-01-09 17:15:39 +0800

    添加QPushButton, QRadioButton, QCheckBox使用示例

--- z_qt_demo/z_qt_demo/src/UIDialog/buttons_widget.cpp
+++ z_qt_demo/z_qt_demo/src/UIDialog/buttons_widget.cpp
@@ -15,12 +15,18 @@ ButtonsWidget::ButtonsWidget(QWidget *parent)
     ui_.setupUi(this);
     setAttribute(Qt::WA_TranslucentBackground);
     setObjectName("ButtonsWidget");
-    initUi();
+    init();
 }
 
 ButtonsWidget::~ButtonsWidget()
 {
 
+}
+
+void ButtonsWidget::init()
+{
+    initUi();
+    initConnect();
 }
 
 void ButtonsWidget::initUi()
@@ -29,4 +35,104 @@ void ButtonsWidget::initUi()
     QRect screen = desktop->screenGeometry();
     resize(screen.width(), screen.height() - TITLE_HEIGHT);
     move(0, TITLE_HEIGHT);
+
+    radioBtnGroup1_ = new QButtonGroup(this);
+    radioBtnGroup1_->addButton(ui_.radioButtonA, 10001);
+    radioBtnGroup1_->addButton(ui_.radioButtonB, 10002);
+    radioBtnGroup1_->addButton(ui_.radioButtonC, 10003);
+
+    radioBtnGroup2_ = new QButtonGroup(this);
+    radioBtnGroup2_->addButton(ui_.radioButton1, 20001);
+    radioBtnGroup2_->addButton(ui_.radioButton2, 20002);
+    radioBtnGroup2_->addButton(ui_.radioButton3, 20003);
+    for (int i = 0; i < radioBtnGroup2_->buttons().size(); ++i)
+    {
+        auto btn = radioBtnGroup2_->buttons()[i];
+        btn->setProperty("group", "RadioBtnGroup2");
+    }
+
+    checkBoxGroup1_.push_back(ui_.checkBoxA);
+    checkBoxGroup1_.push_back(ui_.checkBoxB);
+    checkBoxGroup1_.push_back(ui_.checkBoxC);
+
+    checkBoxGroup2_.push_back(ui_.checkBox1);
+    checkBoxGroup2_.push_back(ui_.checkBox2);
+    checkBoxGroup2_.push_back(ui_.checkBox3);
+    for (int i = 0; i < checkBoxGroup2_.size(); ++i)
+    {
+        auto btn = checkBoxGroup2_[i];
+        btn->setProperty("group", "CheckBoxGroup2");
+    }
+}
+
+void ButtonsWidget::initConnect()
+{
+    connect(ui_.pushButton, &QPushButton::clicked, this, &ButtonsWidget::onPushButtonClicked);
+
+    for (int i = 0; i < radioBtnGroup1_->buttons().size(); ++i)
+    {
+        connect(radioBtnGroup1_->buttons()[i], &QRadioButton::clicked, this, &ButtonsWidget::onGroup1RadionButtonClicked);
+    }
+    for (int i = 0; i < radioBtnGroup2_->buttons().size(); ++i)
+    {
+        connect(radioBtnGroup2_->buttons()[i], &QRadioButton::clicked, this, &ButtonsWidget::onGroup2RadionButtonClicked);
+    }
+
+    for (int i = 0; i < checkBoxGroup1_.size(); ++i)
+    {
+        connect(checkBoxGroup1_[i], &QCheckBox::clicked, this, &ButtonsWidget::onGroup1CheckBoxClicked);
+    }
+    for (int i = 0; i < checkBoxGroup2_.size(); ++i)
+    {
+        connect(checkBoxGroup2_[i], &QCheckBox::clicked, this, &ButtonsWidget::onGroup2CheckBoxClicked);
+    }
+}
+
+void ButtonsWidget::onPushButtonClicked()
+{
+    ui_.textEdit->setText("PushButton");
+}
+
+void ButtonsWidget::onGroup1RadionButtonClicked()
+{
+    auto btn = radioBtnGroup1_->checkedButton();
+    int id = radioBtnGroup1_->checkedId();
+
+    QString text = QString("%1 %2").arg(btn->text()).arg(id);
+    ui_.textEdit->setText(text);
+}
+
+void ButtonsWidget::onGroup2RadionButtonClicked()
+{
+    auto btn = radioBtnGroup2_->checkedButton();
+    int id = radioBtnGroup2_->checkedId();
+
+    QString text = QString("%1 %2").arg(btn->text()).arg(id);
+    ui_.textEdit->setText(text);
+}
+
+void ButtonsWidget::onGroup1CheckBoxClicked()
+{
+    QString text;
+    for (auto btn : checkBoxGroup1_)
+    {
+        if (btn->isChecked())
+        {
+            text += QString("%1 ").arg(btn->text());
+        }
+    }
+    ui_.textEdit->setText(QString("%1").arg(text));
+}
+
+void ButtonsWidget::onGroup2CheckBoxClicked()
+{
+    QString text;
+    for (auto btn : checkBoxGroup2_)
+    {
+        if (btn->isChecked())
+        {
+            text += QString("%1 ").arg(btn->text());
+        }
+    }
+    ui_.textEdit->setText(QString("%1").arg(text));
 }
--- z_qt_demo/z_qt_demo/src/UIDialog/buttons_widget.h
+++ z_qt_demo/z_qt_demo/src/UIDialog/buttons_widget.h
@@ -2,6 +2,8 @@
 
 #include <map>
 #include <string>
+#include <QCheckBox>
+#include <QButtonGroup>
 #include "basedwidget/based_styleshet_widget.h"
 #include "ui_buttons.h"
 
@@ -18,11 +20,23 @@ public:
 
 signals:
 
-public slots:
+public slots :
+    void onPushButtonClicked();
+    void onGroup1RadionButtonClicked();
+    void onGroup2RadionButtonClicked();
+    void onGroup1CheckBoxClicked();
+    void onGroup2CheckBoxClicked();
 
 private:
+    void init();
     void initUi();
+    void initConnect();
 
 private:
     Ui::Buttons ui_;
+    QButtonGroup* radioBtnGroup1_;
+    QButtonGroup* radioBtnGroup2_;
+
+    std::vector<QCheckBox*> checkBoxGroup1_;
+    std::vector<QCheckBox*> checkBoxGroup2_;
 };
