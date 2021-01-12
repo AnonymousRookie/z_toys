513cfdabb1b4517679de8508e386d48cd546e7c0
zhangjie <357688981@qq.com>
2021-01-12 19:13:05 +0800

    ListView中插入结构体数据，点击行显示关联的结构体信息

--- z_qt_demo/z_qt_demo/src/UIDialog/item_views_widget.cpp
+++ z_qt_demo/z_qt_demo/src/UIDialog/item_views_widget.cpp
@@ -4,6 +4,7 @@
 #include <QMenu>
 #include <QLabel>
 #include <QAction>
+#include <QVariant>
 #include <QHBoxLayout>
 #include "item_views_widget.h"
 #include "defines.h"
@@ -16,12 +17,28 @@ ItemViewsWidget::ItemViewsWidget(QWidget *parent)
     ui_.setupUi(this);
     setAttribute(Qt::WA_TranslucentBackground);
     setObjectName("ItemViewsWidget");
-    initUi();
+    init();
 }
 
 ItemViewsWidget::~ItemViewsWidget()
 {
+    uinit();
+}
 
+void ItemViewsWidget::init()
+{
+    initUi();
+    initConnect();
+}
+
+void ItemViewsWidget::uinit()
+{
+    studentInfoListViewModel_->clear();
+    for (auto stu : studentInfos_)
+    {
+        delete stu;
+        stu = nullptr;
+    }
 }
 
 void ItemViewsWidget::initUi()
@@ -30,4 +47,40 @@ void ItemViewsWidget::initUi()
     QRect screen = desktop->screenGeometry();
     resize(screen.width(), screen.height() - TITLE_HEIGHT);
     move(0, TITLE_HEIGHT);
+
+    initStudentInfoListView();
+}
+
+void ItemViewsWidget::initConnect()
+{
+    connect(ui_.listView_studentInfo, SIGNAL(clicked(const QModelIndex&)), this, SLOT(onStudentInfoListViewClicked(const QModelIndex&)));
+}
+
+void ItemViewsWidget::initStudentInfoListView()
+{
+    studentInfoListViewModel_ = new QStandardItemModel(this);
+    ui_.listView_studentInfo->setModel(studentInfoListViewModel_);
+
+    studentInfos_.push_back(new StudentInfo("Jason", 11));
+    studentInfos_.push_back(new StudentInfo("Peter", 22));
+
+    int index = 0;
+    for (auto stu : studentInfos_)
+    {
+        QVariant itemData = QVariant::fromValue<StudentInfo*>(stu);
+        auto item = new QStandardItem(stu->name_);
+        item->setTextAlignment(Qt::AlignCenter);
+        item->setData(itemData);
+        studentInfoListViewModel_->insertRow(index++, item);
+    }
+}
+
+void ItemViewsWidget::onStudentInfoListViewClicked(const QModelIndex& index)
+{
+    auto item = studentInfoListViewModel_->item(index.row());
+    auto itemdata = item->data();
+    StudentInfo* stu = *(StudentInfo**)(itemdata.data());
+
+    QString text = QString("name:%1 age:%2").arg(stu->name_).arg(stu->age_);
+    ui_.textEdit->setText(text);
 }
--- z_qt_demo/z_qt_demo/src/UIDialog/item_views_widget.h
+++ z_qt_demo/z_qt_demo/src/UIDialog/item_views_widget.h
@@ -2,11 +2,22 @@
 
 #include <map>
 #include <string>
+#include <memory>
+#include <QStandardItemModel>
 #include "basedwidget/based_styleshet_widget.h"
 #include "ui_item_views.h"
 
 class QLabel;
 class QMenu;
+
+struct StudentInfo
+{
+    StudentInfo(const QString& name, int age) : name_(name), age_(age) {}
+    QString name_;
+    int age_;
+};
+
+Q_DECLARE_METATYPE(StudentInfo*);
 
 class ItemViewsWidget : public BasedStyleShetWidget
 {
@@ -19,10 +30,18 @@ public:
 signals:
 
 public slots:
+    void onStudentInfoListViewClicked(const QModelIndex& index);
 
 private:
+    void init();
+    void uinit();
     void initUi();
+    void initConnect();
+
+    void initStudentInfoListView();
 
 private:
     Ui::ItemViews ui_;
+    QStandardItemModel* studentInfoListViewModel_ = nullptr;
+    std::vector<StudentInfo*> studentInfos_;
 };
